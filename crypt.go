758a63f713cc52e5bdc9ce3a2fd10a86ad311125
heiskane <henipe@gmail.com>
2020-05-25 19:57:32 +0300

    h1

---
+++ crypt.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+)
+
+func main() {
+	var text string
+	var key string
+	var option bool
+	var result string
+
+	flag.StringVar(&text, "t", "", "Text to encrypt")
+	flag.StringVar(&key, "k", "", "Encryption/Decryption key")
+	flag.BoolVar(&option, "d", false, "Decryption")
+	flag.Parse()
+	
+	if option {
+		for i, letter := range text {
+			if int(letter - rune(key[i % len(key)])) < 0 {
+				result += string(128 + (int(letter - rune(key[i % len(key)])) % 128))
+			} else {
+				result += string(int(letter - rune(key[i % len(key)])) % 128)
+			}
+		}
+	} else {
+		for i, letter := range text {
+			result += string(int(letter + rune(key[i % len(key)])) % 128)
+		}
+	}
+	fmt.Println(result)
+}
